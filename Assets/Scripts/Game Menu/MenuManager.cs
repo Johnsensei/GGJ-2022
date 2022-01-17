391cb2158792359bedebaf7907f0ba1848f3fcce
manoletecu@gmail.com <manoletecu@gmail.com>
2022-01-16 19:26:16 -0500

    Added Main Menu sketch with Quit functionality

---
+++ Assets/Scripts/Game Menu/MenuManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
