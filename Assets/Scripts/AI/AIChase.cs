2d31481dc343c1c87c23be2e9e46acd73e2577ed
Manuel Hermida <manoletecu@gmail.com>
2022-01-18 21:17:36 -0500

    Added AIChase component

---
+++ Assets/Scripts/AI/AIChase.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class AIChase : MonoBehaviour
+{
+    public float ChaseSpeed = 2;
+    [SerializeField]
+    private Transform ChaseTarget;
+
+    void Start()
+    {
+        BeginChasing(ChaseTarget);
+    }
+
+    void Update()
+    {
+        if (ChaseTarget == null)
+            return;
+
+        var chaseDirection = (ChaseTarget.position - transform.position).normalized;
+        transform.position += chaseDirection * ChaseSpeed * Time.deltaTime;
+    }
+
+    public void BeginChasing(Transform chaseTarget)
+    {
+        if (chaseTarget == null)
+            return;
+
+        ChaseTarget = chaseTarget;
+    }
+
+    public void StopChasing()
+    {
+        ChaseTarget = null;
+    }    
+
+}
