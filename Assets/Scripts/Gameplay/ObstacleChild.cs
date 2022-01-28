471eb79f41d316adf81e29a5ca8f4194bd44e106
John Gale <john@johnsensei.com>
2022-01-27 20:47:27 -0600

    Enemy mirrored on robot side

--- Assets/Scripts/Gameplay/ObstacleChild.cs
+++ Assets/Scripts/Gameplay/ObstacleChild.cs
@@ -6,13 +6,19 @@ public class ObstacleChild : MonoBehaviour
 {
     
     public GameObject obstacleParent;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - obstacleParent.transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(
-            obstacleParent.transform.position.x + 5,
-            obstacleParent.transform.position.y,
+            obstacleParent.transform.position.x + offset.x,
+            obstacleParent.transform.position.y + offset.y,
             0);
     }
 }
