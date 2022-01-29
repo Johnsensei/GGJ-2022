b130583cfbb83e21119d745f2366c9e05022d870
John Gale <john@johnsensei.com>
2022-01-29 17:52:37 -0600

    Adds logo and removes start button on main menu - touch screen to start game

---
+++ Assets/Scripts/Scene Loader/StartGame.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+
+    Touch touch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0) {
+            SceneLoader.LoadScene(2);
+        }
+    }
+}
