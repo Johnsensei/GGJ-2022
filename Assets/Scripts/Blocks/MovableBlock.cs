4af4b284abe2986f3d5089a3e99c79306b97dadb
Cory Nanni <35545442+spoofBlue@users.noreply.github.com>
2022-01-30 01:24:59 -0600

    Block assets updated, more enemies added, Block Puzzle added

--- Assets/Scripts/Blocks/MovableBlock.cs
+++ Assets/Scripts/Blocks/MovableBlock.cs
@@ -24,7 +24,7 @@ public class MovableBlock : MonoBehaviour
         _playerLayer = LayerMask.NameToLayer("Robot");
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.layer != _playerLayer)
             return;
